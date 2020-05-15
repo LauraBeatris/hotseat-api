202ef21a1d0d9bfceb593a35c49c649bfeb73092
Laura <laurabeatriserafim@gmail.com>
2020-05-15 08:19:05 -0300

    refactor: create custom respository connected to appointment entity

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,37 +1,16 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-  type: string;
-}
-
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: {
+        date,
+      },
+    });
 
     return findAppointment || null;
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public create({ provider, date, type }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({ provider, date, type });
-
-    this.appointments.push(appointment);
-
-    return appointment;
   }
 }
 
