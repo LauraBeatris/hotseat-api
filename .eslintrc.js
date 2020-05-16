2c9bd8a0127ccb7533f97fa0f11af0746fb921d6
Laura <laurabeatriserafim@gmail.com>
2020-05-16 16:35:54 -0300

    chore: disable eslint camelcase rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
+    '@typescript-eslint/camelcase': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
