4156c401a35811cedb29b17a70ebefe57808e451
Laura <laurabeatriserafim@gmail.com>
2020-05-16 14:40:51 -0300

    feat: add create user service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const findUserWithSameEmail = await usersRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (findUserWithSameEmail) {
+      throw new Error('An user with that email already exists');
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = await usersRepository.create({
+      name,
+      email,
+      password: passwordHash,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
