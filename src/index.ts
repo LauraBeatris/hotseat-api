df12f9bd9a4eeb75fc22fce3fa0e191f1d03d518
Laura <laurabeatriserafim@gmail.com>
2020-05-17 11:02:21 -0300

    feat: add route to retrieve avatar static files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import dotenv from 'dotenv';
-
 import express from 'express';
+
 import routes from './routes';
 
 import './database';
+import uploadConfig from './config/upload';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(8080, () => console.log('🚀 Server Launched'));
