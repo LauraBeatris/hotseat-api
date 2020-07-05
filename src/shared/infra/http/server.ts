492e664229f4d39d277d2ed27a9e697c1300fbc1
Laura <laurabeatriserafim@gmail.com>
2020-07-05 11:06:20 -0300

    fix: update static folder to uploads

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.tmpFolder));
+app.use('/files', express.static(uploadConfig.uploadFolder));
 app.use(routes);
 app.use(errorsHandler);
 
