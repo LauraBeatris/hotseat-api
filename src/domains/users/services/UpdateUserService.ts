03a057fa981684541479620891846c4e64b12e2a
Laura <laurabeatriserafim@gmail.com>
2020-07-09 08:46:41 -0300

    feat: add update user service and unit tests

---
+++ src/domains/users/services/UpdateUserService.ts
@@ -0,0 +1,66 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '@domains/users/infra/database/entities/User';
+import IUsersRepository from '@domains/users/interfaces/IUsersRepository';
+import AppError from '@shared/errors/AppError';
+import IHashProvider from '../providers/HashProvider/interfaces/IHashProvider';
+
+interface IRequest {
+  user_id: string;
+  email: string;
+  name: string;
+  password?: string;
+  old_password?: string;
+}
+
+@injectable()
+class UpdateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  async execute({
+    user_id,
+    email,
+    name,
+    password,
+    old_password,
+  }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    const findUserWithSameEmail = await this.usersRepository.findByEmail(email);
+
+    if (findUserWithSameEmail && findUserWithSameEmail.id !== user_id) {
+      throw new AppError('This email is already being used');
+    }
+
+    user.email = email;
+    user.name = name;
+
+    if (password) {
+      if (!old_password) {
+        throw new AppError(
+          'The old password should provided in order to update the current one',
+        );
+      }
+
+      if (user.password !== old_password) {
+        throw new AppError('The old password provided is invalid');
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    return user;
+  }
+}
+
+export default UpdateUserService;
---
+++ src/domains/users/services/__tests__/UpdateUserService.spec.ts
@@ -0,0 +1,103 @@
+import FakeUsersRepository from '@domains/users/fakes/repositories/FakeUsersRepository';
+import FakeBCryptHashProvider from '@domains/users/providers/HashProvider/fakes/FakeBCryptHashProvider';
+import UpdateUserService from '@domains/users/services/UpdateUserService';
+
+import AppError from '@shared/errors/AppError';
+
+let usersRepository: FakeUsersRepository;
+
+let hashProvider: FakeBCryptHashProvider;
+
+let updateUserService: UpdateUserService;
+
+describe('Upload User Avatar', () => {
+  beforeEach(() => {
+    usersRepository = new FakeUsersRepository();
+
+    hashProvider = new FakeBCryptHashProvider();
+
+    updateUserService = new UpdateUserService(usersRepository, hashProvider);
+  });
+
+  it('should be able to update user', async () => {
+    const {
+      id: user_id,
+      password: old_password,
+    } = await usersRepository.create({
+      name: 'Jackie Chan',
+      email: 'jackiechan@test.com',
+      password: 'meaningless password',
+    });
+
+    const updatedUserData = {
+      user_id,
+      name: 'Updated Jackie Chan',
+      email: 'updatedjackiechan@test.com',
+      old_password,
+      password: 'new meaningless password',
+    };
+
+    const updatedUser = await updateUserService.execute(updatedUserData);
+
+    expect(updatedUser.name).toBe(updatedUserData.name);
+    expect(updatedUser.email).toBe(updatedUserData.email);
+    expect(updatedUser.password).toBe(updatedUserData.password);
+  });
+
+  it("should not update email if it's already being used", async () => {
+    const { id: user_id, name } = await usersRepository.create({
+      name: 'Jackie Chan',
+      email: 'jackiechan@test.com',
+      password: 'meaningless password',
+    });
+
+    const { email: emailAlreadyBeingUsed } = await usersRepository.create({
+      name: 'Jackie Chan Brother',
+      email: 'jackiechanbrother@test.com',
+      password: 'meaningless password',
+    });
+
+    await expect(
+      updateUserService.execute({
+        user_id,
+        name,
+        email: emailAlreadyBeingUsed,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update the password if the old one is not provided', async () => {
+    const { id: user_id, name, email } = await usersRepository.create({
+      name: 'Jackie Chan',
+      email: 'jackiechan@test.com',
+      password: 'meaningless password',
+    });
+
+    await expect(
+      updateUserService.execute({
+        user_id,
+        name,
+        email,
+        password: 'meanless password that should not be saved',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not update password if the old password provided isn't valid", async () => {
+    const { id: user_id, name, email } = await usersRepository.create({
+      name: 'Jackie Chan',
+      email: 'jackiechan@test.com',
+      password: 'meaningless password',
+    });
+
+    await expect(
+      updateUserService.execute({
+        user_id,
+        name,
+        email,
+        old_password: 'invalid old password',
+        password: 'meanless password that should not be saved',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
