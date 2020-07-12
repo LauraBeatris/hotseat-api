301292b5d64898c3daa9cd2c1847edcc5e8a284b
Laura <laurabeatriserafim@gmail.com>
2020-07-12 10:08:59 -0300

    chore(appointments): add constant to store the business start hour

--- src/domains/users/constants/appointments.ts
+++ src/domains/users/constants/appointments.ts
@@ -1,2 +1,11 @@
-// eslint-disable-next-line import/prefer-default-export
+/**
+ * The max number of appointments that the provider is allowed to do per
+ * day according to the bussiness hour range between 08:00AM to 17:00PM
+ */
 export const MAX_APPOINTMENTS_PER_DAY = 10;
+
+/**
+ * The business start hour that the provider is allowed to perform
+ * his/her services
+ */
+export const BUSINESS_START_HOUR = 8;
