a1c4a7e7c28ad6c3f5c74c43438c2cbed00f1bcd
Laura <laurabeatriserafim@gmail.com>
2020-08-18 13:25:36 -0300

    feat: add ses mail provider

---
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,46 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import aws from 'aws-sdk';
+import { injectable, inject } from 'tsyringe';
+
+import IMailProvider from '@shared/container/providers/MailProvider/interfaces/IMailProvider';
+import ISendMailDTO from '@shared/container/providers/MailProvider/dtos/ISendMailDTO';
+import ITemplateProvider from '@shared/container/providers/TemplateProvider/interfaces/ITemplateProvider';
+import { SES_API_VERSION } from '@config/ses';
+import mailConfig from '@config/mail';
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor(
+    @inject('TemplateProvider')
+    private templateProvider: ITemplateProvider,
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: SES_API_VERSION,
+        region: process.env.AWS_DEFAULT_REGION,
+      }),
+    });
+  }
+
+  public async sendMail({
+    to,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
+    const template = await this.templateProvider.parse({
+      templateFilePath: templateData.templateFilePath,
+      variables: templateData.variables,
+    });
+
+    await this.client.sendMail({
+      from: mailConfig.defaults.from,
+      to,
+      subject,
+      html: template,
+    });
+  }
+}
+
+export default SESMailProvider;
