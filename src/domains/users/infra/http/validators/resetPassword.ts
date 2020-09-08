23a8d246b82a19b520f5bb0d80b97aff6892925b
Laura <laurabeatriserafim@gmail.com>
2020-09-08 11:31:51 -0300

    chore: add confirm password field to reset password schema

--- src/domains/users/infra/http/validators/resetPassword.ts
+++ src/domains/users/infra/http/validators/resetPassword.ts
@@ -4,6 +4,7 @@ const resetPasswordValidator = celebrate({
   [Segments.BODY]: {
     password: Joi.string().min(5).required(),
     token: Joi.string().uuid().required(),
+    confirm_password: Joi.any().valid(Joi.ref('password')).required(),
   },
 });
 
